208a979296cd027012bb415aa1db4c04d5f13d6c
Phus Lu <phus.lu@gmail.com>
2011-06-15 17:01:02 +0800

    bugfix

--- portforward.py
+++ portforward.py
@@ -3,30 +3,66 @@
 
 import sys, os, re, time
 import socket, SocketServer, select
+import logging
 
-class PortForwardMixin(object):
-    PORT  = 8000
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(message)s', datefmt='[%d/%b/%Y %H:%M:%S]')
+
+def random_choice(seq):
+    if len(seq) <= 1:
+        return seq[0]
+    else:
+        return seq[int(ord(os.urandom(1))/256.0*len(seq))]
+
+def resolve_netloc(netloc, defaultport=80):
+    if netloc.rfind(':') > netloc.rfind(']'):
+        host, _, port = netloc.rpartition(':')
+        port = int(port)
+    else:
+        host = netloc
+        port = defaultport
+    if host[0] == '[':
+        host = host.strip('[]')
+    return host, port
+
+def socket_forward(local, remote, timeout=60, tick=2, maxping=None, maxpong=None):
+    count = timeout // tick
+    try:
+        while 1:
+            count -= 1
+            (ins, _, errors) = select.select([local, remote], [], [local, remote], tick)
+            if errors:
+                break
+            if ins:
+                for sock in ins:
+                    data = sock.recv(8192)
+                    if data:
+                        if sock is local:
+                            remote.send(data)
+                            count = maxping or timeout // tick
+                        else:
+                            local.send(data)
+                            count = maxpong or timeout // tick
+            if count == 0:
+                break
+    except Exception, ex:
+        logging.warning('socket_forward error=%s', ex)
+
+class ForwardHandler(SocketServer.BaseRequestHandler):
+
     HOSTS = []
 
-class ConnectionHandler(SocketServer.BaseRequestHandler, PortForwardMixin):
-
     def log_message(self, message):
-        address = '%s:%s' % (self.client_address[0] if '.' in self.client_address[0] else '[%s]' % self.client_address[0], self.client_address[1])
-        sys.stdout.write('%s -- [%s] %s\n' % (address, time.ctime(), message))
+        host, port = self.client_address[:2]
+        host = '[%s]' % host if ':' in host else host
+        sys.stdout.write('%s:%s -- [%s] %s\n' % (host, port, time.ctime(), message))
 
     def setup(self):
-        if len(self.HOSTS) > 1:
-            remote_host, remote_port = self.HOSTS[int(ord(os.urandom(1))/256.0*len(self.HOSTS))]
-        else:
-            remote_host, remote_port = self.HOSTS[0]
-        (soc_family, _, _, _, address) = socket.getaddrinfo(remote_host, remote_port)[0]
-        self.remote = socket.socket(soc_family)
-        self.remote.connect(address[:2])
-        self.log_message('Forward to (%r,%r)' % (remote_host, remote_port))
+        host, port = random_choice(self.HOSTS)
+        self.remote = socket.create_connection((host, port))
+        self.log_message('Forward to (%r,%r)' % (host, port))
 
     def handle(self):
-        self._read_write(self.request, self.remote)
-        #self.log_message('Forward End')
+        socket_forward(self.request, self.remote, timeout=300)
 
     def finish(self):
         for soc in (self.request, self.remote):
@@ -35,46 +71,20 @@ class ConnectionHandler(SocketServer.BaseRequestHandler, PortForwardMixin):
             except:
                 pass
 
-    def _read_write(self, soc1, soc2):
-        time_out_max = 60
-        socs = [soc1, soc2]
-        count = 0
-        while 1:
-            count += 1
-            (recv, _, error) = select.select(socs, [], socs, 5)
-            if error:
-                break
-            if recv:
-                for in_ in recv:
-                    data = in_.recv(8192)
-                    if in_ is soc1:
-                        out = soc2
-                    else:
-                        out = soc1
-                    if data:
-                        out.send(data)
-                        count = 0
-            if count == time_out_max:
-                break
-
 if __name__=='__main__':
     if len(sys.argv) == 1:
         print 'usage: portforward.py local remote1 remote2 ...'
-        print 'example: portforward.py 0.0.0.0:8000 127.0.0.1:8001 127.0.0.1:8002'
+        print 'example: portforward.py 0.0.0.0:8080 127.0.0.1:8001 127.0.0.1:8002'
         sys.exit(0)
-    local, remotes = sys.argv[1], sys.argv[2:]
-    localaddr, _, localport = local.rpartition(':')
-    remotehosts = [(x.split(':')[0], int(x.split(':')[1])) for x in remotes]
 
-    PortForwardMixin.PORT = localport
-    PortForwardMixin.HOSTS = remotehosts
+    address = resolve_netloc(sys.argv[1], 8080)
+    hosts = [resolve_netloc(x, 8080) for x in sys.argv[2:]]
 
-    if ':' in localaddr:
+    ForwardHandler.HOSTS = hosts
+
+    if ':' in address[0]:
         SocketServer.TCPServer.address_family = socket.AF_INET6
-        server = SocketServer.ThreadingTCPServer((localaddr.strip('[]'), int(localport)), ConnectionHandler)
-    else:
-        server = SocketServer.ThreadingTCPServer((localaddr, int(localport)), ConnectionHandler)
-
+    server = SocketServer.ThreadingTCPServer(address, ForwardHandler)
     sa = server.socket.getsockname()
     print "Serving Socket on", sa[0], "port", sa[1], "..."
     server.serve_forever()
